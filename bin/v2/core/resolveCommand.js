3d43afe91807c55cf3edcce7b1024c7aa69b9fc8
keshavsoft <nknnkumar@yahoo.com>
2026-05-21 12:31:43 +0530

    buildHeader

--- bin/v2/core/resolveCommand.js
+++ bin/v2/core/resolveCommand.js
@@ -1,12 +1,14 @@
 import init from "../commands/init.js";
 import addListeners from "../commands/addListeners.js";
 import htmlIdClick from "../commands/htmlIdClick.js";
+import buildHeader from "../commands/buildHeader.js";
 
 // resolveCommand.js
 const map = {
     init,
     addListeners,
-    htmlIdClick
+    htmlIdClick,
+    buildHeader
 };
 
 export default function resolveCommand(cmd) {
